d0bc64ed82d8c1f68228d8711a2d8f98a1a2699d
nadia356 <110473332+nadia356@users.noreply.github.com>
2022-10-05 12:16:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/splash/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button btn1=findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
